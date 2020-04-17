b39a3eca063f98beae939a8b10410b561417a3e3
Alexandre Mendes <alexandregme@gmail.com>
2020-04-17 17:34:39 -0300

    [next] configuration

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-
-module.exports = {
-    entry: './components/index.js',
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js'
-    }
-};
